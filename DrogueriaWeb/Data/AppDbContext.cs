5d334c5f4f377c0555d13b3fdbe8b8181772d2d3
alberto florez <alberto florez@DESKTOP-AHQNJ2N>
2025-12-23 22:16:36 -0500

    data models forlder ok

---
+++ DrogueriaWeb/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DrogueriaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrogueriaWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Producto> Productos => Set<Producto>();
+    }
+}
